d1f67c9bb04821438b3aad63d9718f721321d606
Planck1043 <zhao1036713209@gmail.com>
2017-12-15 17:56:15 +0800

    implement Notice CRUD

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -12,7 +12,38 @@ class NoticesController < ApplicationController
       :title => params[:notice][:title],
       :text => params[:notice][:text]
       )
-    @notice.save
+
+    if @notice.save
+      redirect_to notices_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @notice = Notice.find(params[:id])
+  end
+
+  def edit
+    @notice = Notice.find(params[:id])
+  end
+
+  def update
+    @notice = Notice.find(params[:id])
+
+    if @notice.update(
+      :title => params[:notice][:title],
+      :text => params[:notice][:text]
+      )
+      redirect_to notice_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @notice = Notice.find(params[:id])
+    @notice.destroy
 
     redirect_to notices_path
   end
